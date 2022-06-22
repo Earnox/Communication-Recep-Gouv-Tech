6bd16d1da6af456b5753672af1e4a44393239c3b
Earnox <stevenson74@hotmail.fr>
2022-06-22 16:38:05 +0200

    added a function to set the info in bdd withe variable

--- backEnd/instaleur/editBDDinterventiontech/interventionTech.php
+++ backEnd/instaleur/editBDDinterventiontech/interventionTech.php
@@ -2,9 +2,13 @@
 
 class nouvelleInterventionTech
 {
-public string $lieu ;
-public string $nature_Intevention;
-public string $risque;
-public string $info_statut_app ;
+    public string $id = "2";
+    public string $lieu ;
+    public string $nature_Intevention;
+    public string $risque;
+    public string $info_statut_app ;
+    public function getIntervention(): array
+    {
 
+    }
 }
--- front-end/mainpages/index.php
+++ front-end/mainpages/index.php
@@ -3,9 +3,10 @@ require_once '../header/header.php';
 require_once '../header/navbar.php';
 
 ?>
-<html>
-
+<!DOCTYPE html>
+<html lang="fr">
 <head>
+    <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <link rel="stylesheet" href="./css/style.css">
@@ -13,8 +14,16 @@ require_once '../header/navbar.php';
 
 <body>
   <header>
-    <?php echo $title ?> <?php echo $navbar ?> </header>
+    <?php
 
+    echo $title ?> <?php
+
+    echo $navbar ?> </header>
+  <form action="action.php" method="post">
+      <p>lieu: <input type="text" name="lieu" /></p>
+      <p>nature Intevention: <input type="text" name="nature_Intevention" /></p>
+      <p><input type="submit" value="OK"></p>
+  </form>
 
   <div class="container">
     <div class="row">
