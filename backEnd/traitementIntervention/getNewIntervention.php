6b1477121a22667ff3c0b1dcec26c6361dce281c
Earnox <stevenson74@hotmail.fr>
2022-06-23 18:23:02 +0200

    adde function and files to display the info of bdd

--- backEnd/traitementIntervention/getNewIntervention.php
+++ backEnd/traitementIntervention/getNewIntervention.php
@@ -1,10 +1,7 @@
 <?php
 // funtion to add a new intervention tech
-require_once '../instaleur/editBDDinterventiontech/interventionTech.php';
+require_once '../instaleur/editBDDinterventiontechOld/interventionTech.php';
 // script.php.
-
-
-
 
 try {
 
@@ -12,7 +9,8 @@ try {
     var_dump($nvintervention->getIntervention($_POST));
     var_dump($_POST);
     $pdoBDdinterventionEnCour = new PDO('mysql:host=localhost:3307;dbname=interventionTechnique;', 'root', '');
-    $newInter = $pdoBDdinterventionEnCour->prepare("INSERT INTO interventionencour( 
+    $newInter = $pdoBDdinterventionEnCour->prepare(
+        "INSERT INTO interventionencour( 
   postEmployer,
    lieu,
     nature_Intevention,
@@ -21,7 +19,7 @@ try {
     date_intevention,
     remarque_intervention,              
     info_statut_app,
-    statut_intervention                          ) 
+    statut_intervention ) 
     VALUES( :postEmployer,
    :lieu ,
     :nature_Intevention,
@@ -31,7 +29,9 @@ try {
     :remarque_intervention,              
     :info_statut_app,
     :statut_intervention )
-    ");
+    "
+    );
+
     $newInter->bindValue(':postEmployer', $nvintervention->postEmployer, PDO::PARAM_STR);
     $newInter->bindValue(':lieu', $nvintervention->lieu, PDO::PARAM_STR);
     $newInter->bindValue(':nature_Intevention', $nvintervention->nature_Intevention, PDO::PARAM_STR);
@@ -44,12 +44,7 @@ try {
     $newInter->bindValue(':statut_intervention', $nvintervention->statut_intervention, PDO::PARAM_STR);
     var_dump($newInter->execute());
     return $newInter->fetchAll();
-
 } catch (PDOException $error) {
     echo 'vous avez une erreur lors de l\'insetion d\'informatior veuilliez contacter le support <br>  ' . PHP_EOL;
     file_put_contents('error_Logs_Instalation.log', 'Probleme insetion BDD' . $error->getMessage() . PHP_EOL, FILE_APPEND);
-
 }
-
-?>
-
--- front-end/mainpages/index.php
+++ front-end/mainpages/index.php
@@ -1,12 +1,13 @@
 <?php
 require_once '../header/header.php';
 require_once '../header/navbar.php';
-
+require_once '../../backEnd/traitementIntervention/getInterventionInProgress.php'
 ?>
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
-    <meta charset="UTF-8">
+  <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <link rel="stylesheet" href="./css/style.css">
@@ -18,44 +19,60 @@ require_once '../header/navbar.php';
 
     echo $title ?> <?php
 
-    echo $navbar ?> </header>
+                    echo $navbar ?> </header>
   <form action="../../backEnd/traitementIntervention/getNewIntervention.php" method="post">
-      <p>post: <input type="text" name="lieu" /></p>
-      <p>lieu: <input type="text" name="postEmployer" /></p>
-      <p>nature Intevention: <input type="text" name="nature_Intevention" /></p>
-      <p>risque: <input type="text" name="risque" /></p>
-      <p>dateDemandeIntervention: <input type="date" name="dateDemandeIntervention" /></p>
-      <p>date_intevention: <input type="date" name="date_intevention" /></p>
-      <p>remarque: <input type="remarque_intervention" name="remarque_intervention" /></p>
-      <p>info_statut_app: <input type="text" name="info_statut_app" /></p>
-      <p>statut_intervention: <input type="text" name="statut_intervention" /></p>
-      <p><input type="submit" value="OK"></p>
+    <p>post: <input type="text" name="lieu" /></p>
+    <p>lieu: <input type="text" name="postEmployer" /></p>
+    <p>nature Intevention: <input type="text" name="nature_Intevention" /></p>
+    <p>risque: <input type="text" name="risque" /></p>
+    <p>dateDemandeIntervention: <input type="date" name="dateDemandeIntervention" /></p>
+    <p>date_intevention: <input type="date" name="date_intevention" /></p>
+    <p>remarque: <input type="remarque_intervention" name="remarque_intervention" /></p>
+    <p>info_statut_app: <input type="text" name="info_statut_app" /></p>
+    <p>statut_intervention: <input type="text" name="statut_intervention" /></p>
+    <p><input type="submit" value="OK"></p>
   </form>
+  <main>
+    <table>
+      <?php getAllInterProgress();
+      ?>
 
-  <div class="container">
-    <div class="row">
-      <div class="col-md-12">
-        <h1>Welcome to the home page</h1>
-      </div>
+      <thead>
+        <tr>
+          <th>Id</th>
+          <th>Date</th>
+          <th>Poste</th>
+          <th>Appartement</th>
+          <th>Nature de l'intervention</th>
+          <th>Risque</th>
+          <th>info Menage</th>
+          <th>Date de réalisation</th>
+          <th>Remarques</th>
+          <th>Statut</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php $getAllInterProgress = getAllInterProgress();
+        foreach ($getAllInterProgress as $inters) {
+        ?>
+        <tr>
+          <?php
+            foreach ($inters as $inter) {
+            ?>
 
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. A neque, debitis doloremque ex ipsam
-        voluptatibus. Cum est quis, aspernatur eaque perferendis, adipisci at, quisquam similique perspiciatis nobis
-        error reiciendis facilis!</p>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. A neque, debitis doloremque ex ipsam
-        voluptatibus. Cum est quis, aspernatur eaque perferendis, adipisci at, quisquam similique perspiciatis nobis
-        error reiciendis facilis!</p>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. A neque, debitis doloremque ex ipsam
-        voluptatibus. Cum est quis, aspernatur eaque perferendis, adipisci at, quisquam similique perspiciatis nobis
-        error reiciendis facilis!</p>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. A neque, debitis doloremque ex ipsam
-        voluptatibus. Cum est quis, aspernatur eaque perferendis, adipisci at, quisquam similique perspiciatis nobis
-        error reiciendis facilis!</p>
-      <p>
+          <td>
+            <?php echo $inter ?>
+          </td>
+          <?php
+            }
+            ?>
+        <tr>
+          <?php
+        } ?>
+        </tr>
 
-      </p>
-
-    </div>
-  </div>
+      </tbody>
+    </table>
 </body>
 
 </html>
